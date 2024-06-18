e49bd265a506bf9e6b857b420beb2b8065b3bcaa
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2024-06-18 17:44:08 -0600

    changed about me section

--- src/App.tsx
+++ src/App.tsx
@@ -76,22 +76,31 @@ function App() {
 
       <section className="light-green grades-section">
         <div className="school-section">
-          <div className="school-part-of-section">
-            <section className="description-section hidden">
-              <section className="hidden">
-                <h2>University of British Columbia</h2>
-                <img
-                  src={UBC}
-                  alt="ubc logo"
-                  height="100px"
-                  style={{ marginLeft: "-20px" }}
-                />
-              </section>
-            </section>
-          </div>
+          <h1 className="hidden about">About me:</h1>
+          <div></div>
 
+          <section className="school-part-of-section">
+            <div className="description-section hidden">
+              {/* <h2>University of British Columbia</h2>
+              <img src={UBC} alt="ubc logo" height="100px" /> */}
+              <h2>Hi I'm Connor!</h2>
+              <p>I'm a software engineer from Calgary, Alberta.</p>
+              <p>
+                I’m a third year student at the University of British Columbia
+                studying computer science. Looking for software engineer
+                internships!
+              </p>
+              <p>
+                I enjoy connectioning with people, exploring new technology, and
+                developing cool projects with a particular interest in fullstack
+                development. Outside of tech - you can find me competing in
+                sports such as volleyball and bouldering, hiking up mountains
+                with friends, or just listening to good music 😄
+              </p>
+            </div>
+          </section>
           <section className="working-on-section hidden">
-            <h2>Currently working on</h2>
+            <h2>I'm currently working on</h2>
             <section className="text-section2">
               <h3>Path Finder</h3>
               <p className="hidden">
@@ -109,7 +118,7 @@ function App() {
           </section>
           <div className="interests-section">
             <h2 className="center hidden" style={{ marginTop: "20px" }}>
-              Parts of my life:
+              Parts of my life
             </h2>
             <section className="interest-list">
               <div className="interested-buttons hidden">
