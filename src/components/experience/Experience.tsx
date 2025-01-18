97c3f21a86f9ad8b3473280a3f6c194f9ea94f83
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2025-01-17 17:04:28 -0800

    updated formatting

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -32,7 +32,7 @@ const Experience = () => {
               <h2 className="role">{job.employer} </h2>
               <h3 className="employer"> — {job.role}</h3>
             </div>
-            <p className="languages">{job.languages}</p>
+            <p className="languages">Software: {job.languages}</p>
             <h3 className="description">{job.description}</h3>
             {job.id === ExperienceJSON.length ? null : (
               <div className="vertical-line"></div>
--- src/tools/ExperienceJSON.tsx
+++ src/tools/ExperienceJSON.tsx
@@ -11,7 +11,7 @@ export const ExperienceJSON: Experience[] = [
     id: 1,
     role: "Software Developer",
     employer: "UBC Learning Centre",
-    languages: "Python | JavaScript",
+    languages: "Python | JavaScript | pandas | bs4",
     description:
       "Developed software solutions for 3,500+ students and 20+ professors, streamlining course organization and reducing manual effort. Automated the management of 180+ class requirements across 12 majors using Python, Beautiful Soup, and pandas. Built interactive learning tools for 700+ students with JavaScript and SCORM API, including a quiz-integrated crossword puzzle. Collaborated with professors to create custom quiz software, manage faculty data, and provide IT support.",
   },
@@ -19,7 +19,7 @@ export const ExperienceJSON: Experience[] = [
     id: 2,
     role: "AP Computer Science Tutor",
     employer: "BeWise Education",
-    languages: "Java | Object Oriente Programming",
+    languages: "Java",
     description:
       "Tutored AP students in Java and object-oriented programming, adapting lessons and feedback to boost overall grades by 20%. Guided students in mastering data structures and algorithms with clear, simplified explanations to ensure understanding and progress.",
   },
