03ce5ac97c5623f6d21fc70c1aca00ccbe4b9d65
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2025-03-24 16:17:53 -0700

    updated experience

--- src/tools/ExperienceJSON.tsx
+++ src/tools/ExperienceJSON.tsx
@@ -9,14 +9,21 @@ export interface Experience {
 export const ExperienceJSON: Experience[] = [
   {
     id: 1,
-    role: "Software Developer",
+    role: "Software Engineer Intern",
+    employer: "UBC Learning Centre",
+    languages: "TBA",
+    description: "Incoming...",
+  },
+  {
+    id: 2,
+    role: "Software Engineer",
     employer: "UBC Learning Centre",
     languages: "Python | JavaScript | Apache Superset | pandas | bs4",
     description:
       "Developed software solutions for 1000+ students and 20+ professors, streamlining course organization and reducing manual effort. Automated the management of 100+ class requirements across 12 majors using Python, Beautiful Soup, and pandas. Built interactive learning tools for 700+ students with JavaScript and SCORM API, including a quiz-integrated crossword puzzle. Collaborated with professors to create custom quiz software, manage faculty data, and provide IT support.",
   },
   {
-    id: 2,
+    id: 3,
     role: "AP Computer Science Tutor",
     employer: "BeWise Education",
     languages: "Java",
@@ -24,7 +31,7 @@ export const ExperienceJSON: Experience[] = [
       "Tutored AP students in Java and object-oriented programming, adapting lessons and feedback to boost overall grades by 20%. Guided students in mastering data structures and algorithms with clear, simplified explanations to ensure understanding and progress.",
   },
   {
-    id: 3,
+    id: 4,
     role: "Hackathon Mentor",
     employer: "nwPlus HackCamp",
     languages: "React | Firebase | Git",
