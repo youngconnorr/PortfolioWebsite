bc3874d139c85b517d23710790287a98c0f20e73
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2025-03-24 16:22:05 -0700

    updated

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { ExperienceJSON } from "../../tools/ExperienceJSON";
 
 const Experience = () => {
-
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
@@ -33,7 +32,7 @@ const Experience = () => {
               <h2 className="role">{job.employer} </h2>
               <h3 className="employer"> — {job.role}</h3>
             </div>
-            <p className="languages">Software: {job.languages}</p>
+            <p className="languages">{job.languages}</p>
             <h3 className="description">{job.description}</h3>
             {job.id === ExperienceJSON.length ? null : (
               <div className="vertical-line"></div>
--- src/tools/ExperienceJSON.tsx
+++ src/tools/ExperienceJSON.tsx
@@ -11,22 +11,23 @@ export const ExperienceJSON: Experience[] = [
     id: 1,
     role: "Software Engineer Intern",
     employer: "UBC Learning Centre",
-    languages: "TBA",
+    languages: " ",
     description: "Incoming...",
   },
   {
     id: 2,
-    role: "Software Engineer",
+    role: "Software Engineer [Part-Time]",
     employer: "UBC Learning Centre",
-    languages: "Python | JavaScript | Apache Superset | pandas | bs4",
+    languages:
+      "Python | JavaScript | PostgreSQL | Apache Superset | pandas | bs4",
     description:
-      "Developed software solutions for 1000+ students and 20+ professors, streamlining course organization and reducing manual effort. Automated the management of 100+ class requirements across 12 majors using Python, Beautiful Soup, and pandas. Built interactive learning tools for 700+ students with JavaScript and SCORM API, including a quiz-integrated crossword puzzle. Collaborated with professors to create custom quiz software, manage faculty data, and provide IT support.",
+      "Developed educational software for 2500+ students and 20+ professors. Automated the management of 250+ class requirements across 12 majors using Python, Beautiful Soup. Created an ETL pipeline of 500+ lines of faculty data using Python PostgreSQL, and Apache Superset. Built interactive learning tools for 700+ students with JavaScript and SCORM API, including a quiz-integrated crossword puzzle. Collaborated with professors to create custom quiz software, manage faculty data, and provide IT support.",
   },
   {
     id: 3,
     role: "AP Computer Science Tutor",
     employer: "BeWise Education",
-    languages: "Java",
+    languages: "Java | Object Oriented Programming",
     description:
       "Tutored AP students in Java and object-oriented programming, adapting lessons and feedback to boost overall grades by 20%. Guided students in mastering data structures and algorithms with clear, simplified explanations to ensure understanding and progress.",
   },
